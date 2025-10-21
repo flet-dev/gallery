5673bad4e7e1212d352d68d6fdfd2d0b233af844
Feodor Fitsner <feodor@appveyor.com>
2025-10-21 13:18:05 -0700

    Update 05_column_scroll_to.py

--- src/examples/layout/column/05_column_scroll_to.py
+++ src/examples/layout/column/05_column_scroll_to.py
@@ -4,58 +4,62 @@ name = "Controlling scroll position for Column"
 
 
 def example():
-    cl = ft.Column(
-        spacing=10,
-        height=180,
-        width=300,
-        scroll=ft.ScrollMode.ALWAYS,
-        controls=[
-            ft.Container(
-                ft.Text("Section A"),
-                alignment=ft.Alignment.TOP_LEFT,
-                bgcolor=ft.Colors.YELLOW_200,
-                height=100,
-                key=ft.ScrollKey("A"),
-            ),
-            ft.Container(
-                ft.Text("Section B"),
-                alignment=ft.Alignment.TOP_LEFT,
-                bgcolor=ft.Colors.GREEN_200,
-                height=100,
-                key=ft.ScrollKey("B"),
-            ),
-            ft.Container(
-                ft.Text("Section C"),
-                alignment=ft.Alignment.TOP_LEFT,
-                bgcolor=ft.Colors.BLUE_200,
-                height=100,
-                key=ft.ScrollKey("C"),
-            ),
-            ft.Container(
-                ft.Text("Section D"),
-                alignment=ft.Alignment.TOP_LEFT,
-                bgcolor=ft.Colors.PINK_200,
-                height=100,
-                key=ft.ScrollKey("D"),
-            ),
-        ],
-    )
+    cl = ft.Ref[ft.Column]()
 
-    def scroll_to_a(_):
-        cl.scroll_to(scroll_key="A", duration=1000)
+    async def scroll_to_a(_):
+        await cl.current.scroll_to(scroll_key="A", duration=1000)
 
-    def scroll_to_b(_):
-        cl.scroll_to(scroll_key="B", duration=1000)
+    async def scroll_to_b(_):
+        await cl.current.scroll_to(scroll_key="B", duration=1000)
 
-    def scroll_to_c(_):
-        cl.scroll_to(scroll_key="C", duration=1000)
+    async def scroll_to_c(_):
+        await cl.current.scroll_to(scroll_key="C", duration=1000)
 
-    def scroll_to_d(_):
-        cl.scroll_to(scroll_key="D", duration=1000)
+    async def scroll_to_d(_):
+        await cl.current.scroll_to(scroll_key="D", duration=1000)
 
     return ft.Column(
         [
-            ft.Container(cl, border=ft.Border.all(1)),
+            ft.Container(
+                ft.Column(
+                    ref=cl,
+                    spacing=10,
+                    height=180,
+                    width=300,
+                    scroll=ft.ScrollMode.ALWAYS,
+                    controls=[
+                        ft.Container(
+                            ft.Text("Section A"),
+                            alignment=ft.Alignment.TOP_LEFT,
+                            bgcolor=ft.Colors.YELLOW_200,
+                            height=100,
+                            key=ft.ScrollKey("A"),
+                        ),
+                        ft.Container(
+                            ft.Text("Section B"),
+                            alignment=ft.Alignment.TOP_LEFT,
+                            bgcolor=ft.Colors.GREEN_200,
+                            height=100,
+                            key=ft.ScrollKey("B"),
+                        ),
+                        ft.Container(
+                            ft.Text("Section C"),
+                            alignment=ft.Alignment.TOP_LEFT,
+                            bgcolor=ft.Colors.BLUE_200,
+                            height=100,
+                            key=ft.ScrollKey("C"),
+                        ),
+                        ft.Container(
+                            ft.Text("Section D"),
+                            alignment=ft.Alignment.TOP_LEFT,
+                            bgcolor=ft.Colors.PINK_200,
+                            height=100,
+                            key=ft.ScrollKey("D"),
+                        ),
+                    ],
+                ),
+                border=ft.Border.all(1),
+            ),
             ft.Column(
                 [
                     ft.Text("Scroll to:"),
