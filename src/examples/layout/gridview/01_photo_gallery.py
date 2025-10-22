592d8bcc2d42805781340be60d8c0f547631e025
InesaFitsner <inesa@appveyor.com>
2025-10-22 10:31:01 -0700

    Refactor GridView to use controls argument
    Simplifies the photo gallery example by passing the list of Image controls directly to the GridView's 'controls' argument instead of appending them after instantiation. The previous approach is now commented out for reference.

--- src/examples/layout/gridview/01_photo_gallery.py
+++ src/examples/layout/gridview/01_photo_gallery.py
@@ -4,7 +4,27 @@ name = "GridView Example"
 
 
 def example():
-    images = ft.GridView(
+    # images = ft.GridView(
+    #     height=400,
+    #     width=400,
+    #     runs_count=5,
+    #     max_extent=150,
+    #     child_aspect_ratio=1.0,
+    #     spacing=5,
+    #     run_spacing=5,
+    # )
+
+    # for i in range(0, 60):
+    #     images.controls.append(
+    #         ft.Image(
+    #             src=f"https://picsum.photos/150/150?{i}",
+    #             fit=ft.BoxFit.NONE,
+    #             repeat=ft.ImageRepeat.NO_REPEAT,
+    #             border_radius=ft.BorderRadius.all(10),
+    #         )
+    #     )
+
+    return ft.GridView(
         height=400,
         width=400,
         runs_count=5,
@@ -12,16 +32,13 @@ def example():
         child_aspect_ratio=1.0,
         spacing=5,
         run_spacing=5,
-    )
-
-    for i in range(0, 60):
-        images.controls.append(
+        controls=[
             ft.Image(
                 src=f"https://picsum.photos/150/150?{i}",
                 fit=ft.BoxFit.NONE,
                 repeat=ft.ImageRepeat.NO_REPEAT,
                 border_radius=ft.BorderRadius.all(10),
             )
-        )
-
-    return images
+            for i in range(0, 60)
+        ],
+    )
