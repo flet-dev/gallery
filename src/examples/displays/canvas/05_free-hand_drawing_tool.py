fcf785294968542cb0b25bc8bc80349b359bc176
InesaFitsner <inesa@appveyor.com>
2025-10-25 10:28:05 -0700

    Refactor free-hand drawing tool to use state hooks
    Replaces manual state management with Flet's use_state hooks for tracking drawn items and last pointer position. Introduces an Item class for line segments and updates event handlers to append new lines using state, improving code clarity and reactivity.

--- src/examples/displays/canvas/05_free-hand_drawing_tool.py
+++ src/examples/displays/canvas/05_free-hand_drawing_tool.py
@@ -3,33 +3,37 @@ import flet as ft
 name = "Free-hand drawing tool"
 
 
-def example():
-    import math
+# @ft.observable
+# @dataclass
+class Item:
+    def __init__(self, x1: float, y1: float, x2: float, y2: float):
+        self.x1 = float(x1)
+        self.y1 = float(y1)
+        self.x2 = float(x2)
+        self.y2 = float(y2)
 
+
+def example():
     import flet.canvas as cv
 
-    class State:
-        x: float
-        y: float
-
-    state = State()
+    items, set_items = ft.use_state((list[Item]))
+    last_pos, set_last_pos = ft.use_state((None, None))
 
     def pan_start(e: ft.DragStartEvent):
-        state.x = e.local_x
-        state.y = e.local_y
+        # state.x = e.local_x
+        # state.y = e.local_y
+        set_last_pos((e.local_position.x, e.local_position.y))
+        print(f"Pan start at: {e.local_position.x}, {e.local_position.y}")
 
     def pan_update(e: ft.DragUpdateEvent):
-        cp.shapes.append(
-            cv.Line(
-                state.x, state.y, e.local_x, e.local_y, paint=ft.Paint(stroke_width=3)
-            )
+        set_items(
+            lambda cur: cur
+            + [Item(last_pos[0], last_pos[1], e.local_position.x, e.local_position.y)]
         )
-        cp.update()
-        state.x = e.local_x
-        state.y = e.local_y
+        set_last_pos((e.local_position.x, e.local_position.y))
 
     cp = cv.Canvas(
-        [
+        shapes=[
             cv.Fill(
                 ft.Paint(
                     gradient=ft.PaintLinearGradient(
@@ -37,6 +41,17 @@ def example():
                     )
                 )
             ),
+        ]
+        + [
+            cv.Line(
+                item.x1,
+                item.y1,
+                item.x2,
+                item.y2,
+                paint=ft.Paint(stroke_width=3),
+                key=item.index,
+            )
+            for item in items
         ],
         content=ft.GestureDetector(
             on_pan_start=pan_start,
