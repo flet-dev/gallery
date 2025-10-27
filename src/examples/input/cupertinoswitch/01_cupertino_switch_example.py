d519ed6762f69d97c8f78884646ab016a83e43de
InesaFitsner <inesa@appveyor.com>
2025-10-27 12:45:11 -0700

    Add CupertinoSwitch input examples
    Introduces example usage of CupertinoSwitch, Material Switch, and adaptive Switch in Flet. Includes descriptive metadata for the CupertinoSwitch example.

---
+++ src/examples/input/cupertinoswitch/01_cupertino_switch_example.py
@@ -0,0 +1,26 @@
+import flet as ft
+
+name = "CupertinoSwitch example"
+
+
+def example():
+    c1 = ft.CupertinoSwitch(
+        label="Cupertino Switch",
+        value=True,
+    )
+    c2 = ft.Switch(
+        label="Material Switch",
+        value=True,
+    )
+
+    c3 = ft.Switch(
+        adaptive=True,
+        label="Adaptive Switch",
+        value=True,
+        tooltip=ft.Tooltip(
+            size_constraints=ft.BoxConstraints(max_width=300),
+            message="Adaptive Switch shows as CupertinoSwitch on macOS and iOS and as Switch on other platforms",
+        ),
+    )
+
+    return ft.Column(controls=[c1, c2, c3])
